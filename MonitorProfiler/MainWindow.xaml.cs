f0b78aa2815a22a26f1d38e80cd640e5739bcff5
r4dius <r4dius@gmail.com>
2017-06-01 12:39:27 +0200

    Nothin'

--- MonitorProfiler/MainWindow.xaml.cs
+++ MonitorProfiler/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media.Imaging;
 using MonitorProfiler.GUI;
 using MonitorProfiler.Win32;
 using MonitorProfiler.Models.Display;
